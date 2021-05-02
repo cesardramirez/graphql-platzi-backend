aab8df73d3463cc7e15d6cee1ac2b9cf01aa99fa
cesardramirez <cesardavid89@gmail.com>
2021-05-02 18:03:11 -0500

    feat(custom-types): Separate schema and resolvers into files.

--- index.js
+++ index.js
@@ -6,27 +6,17 @@ const { buildSchema } = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-// Definiendo el esquema (con 2 queries).
+// Definiendo el esquema (con las queries del archivo).
 const schema = buildSchema(
   readFileSync(
     join(__dirname, 'lib', 'schema.graphql'), 'utf-8'
   )
 )
-
-// Configurar los resolvers.
-// Se define los resolvers (una función) por cada query.
-const resolvers = {
-  hello: () => {
-    return 'Hola Mundo!'
-  },
-  gretting: () => {
-    return 'Saludo a todos!'
-  }
-}
 
 // Se ejecutan cada uno de los queries.
 app.use('/graphql', graphqlHTTP({
